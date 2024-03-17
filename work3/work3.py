fed893d599aad284b6be3be08727c7c8e6cb4b11
SurFace81 <sur.f4ce@yandex.ru>
2024-03-17 19:44:13 +0300

    work4

--- work3/work3.py
+++ work3/work3.py
@@ -142,70 +142,70 @@ def evaluate_spline(x, y, b, c, d, x_new):
 # ===========================
 data = tools.read_file()
 
-# lagrange interp.
-start = 1889
-y = [data[i][5] for i in range(start, start + 12)]
-x = np.linspace(0, 12, 12)
-x_interp = np.arange(min(x), max(x) + 0.1, 0.1)
-y_interp = [lagrange_interp(x, y, xi) for xi in x_interp]
-tools.plot(x, y, x_interp, y_interp, "(Lagrange) y: [" + '; '.join(str(_) for _ in y) + "]")
+# # lagrange interp.
+# start = 1889
+# y = [data[i][5] for i in range(start, start + 12)]
+# x = np.linspace(0, 12, 12)
+# x_interp = np.arange(min(x), max(x) + 0.1, 0.1)
+# y_interp = [lagrange_interp(x, y, xi) for xi in x_interp]
+# tools.plot(x, y, x_interp, y_interp, "(Lagrange) y: [" + '; '.join(str(_) for _ in y) + "]")
 
 
-# newthon interp. ver. 1
-y1 = y[:6]
-x1 = np.linspace(0, 6, 6)
-x_interp = np.arange(min(x1), max(x1) + 0.1, 0.1)
-y_interp = [newton_interp(x1, y1, xi) for xi in x_interp]
-tools.plot(x1, y1, x_interp, y_interp, "(Newthon ver. 1) y: [" + '; '.join(str(_) for _ in y1) + "]")
+# # newthon interp. ver. 1
+# y1 = y[:6]
+# x1 = np.linspace(0, 6, 6)
+# x_interp = np.arange(min(x1), max(x1) + 0.1, 0.1)
+# y_interp = [newton_interp(x1, y1, xi) for xi in x_interp]
+# tools.plot(x1, y1, x_interp, y_interp, "(Newthon ver. 1) y: [" + '; '.join(str(_) for _ in y1) + "]")
 
 
-# newthon interp. ver. 2
-y2 = y[6:]
-x2 = np.linspace(0, 6, 6)
-x_interp = np.arange(min(x2), max(x2) + 0.1, 0.1)
-y_interp = [newton_interp2(x2, y2, xi) for xi in x_interp]
-tools.plot(x2, y2, x_interp, y_interp, "(Newthon ver. 2) y: [" + '; '.join(str(_) for _ in y2) + "]")
+# # newthon interp. ver. 2
+# y2 = y[6:]
+# x2 = np.linspace(0, 6, 6)
+# x_interp = np.arange(min(x2), max(x2) + 0.1, 0.1)
+# y_interp = [newton_interp2(x2, y2, xi) for xi in x_interp]
+# tools.plot(x2, y2, x_interp, y_interp, "(Newthon ver. 2) y: [" + '; '.join(str(_) for _ in y2) + "]")
 
 
-# approx. with 5th degree polynome
-y_all = [data[1875 + i][5] for i in range(len(data)) if data[1875 + i][5] != 999.9]
-n = len(y_all)
-x3 = np.linspace(0, n, n)
+# # approx. with 5th degree polynome
+# y_all = [data[1875 + i][5] for i in range(len(data)) if data[1875 + i][5] != 999.9]
+# n = len(y_all)
+# x3 = np.linspace(0, n, n)
 
-fig, axs = plt.subplots(nrows=1, ncols=5, sharex=True, sharey=True)
-for d, ax in zip(range(1, 6), axs.flat):
-    coefficients = poly_approx(x3, y_all, d)
-    x_interp = np.arange(min(x3), max(x3) + 0.1, 0.1)
-    y_interp = [poly_val(coefficients, xi) for xi in x_interp]
-    ax.plot(x3, y_all, 'o')
-    ax.plot(x_interp, y_interp, 'red')
-    ax.set_title('Degree {}'.format(d))
-plt.show()
+# fig, axs = plt.subplots(nrows=1, ncols=5, sharex=True, sharey=True)
+# for d, ax in zip(range(1, 3), axs.flat):
+#     coefficients = poly_approx(x3, y_all, d)
+#     x_interp = np.arange(min(x3), max(x3) + 0.1, 0.1)
+#     y_interp = [poly_val(coefficients, xi) for xi in x_interp]
+#     ax.plot(x3, y_all, 'o')
+#     ax.plot(x_interp, y_interp, 'red')
+#     ax.set_title('Degree {}'.format(d))
+# plt.show()
 
-plt.plot(x3, y_all, "o")
-plt.plot(x_interp, y_interp, "red")
-plt.title("Approximation ({} degree)".format(5))
-plt.show()
-
-
-# cubic spline interp.
-y3 = y
-x3 = np.linspace(0, 12, 12)
-b, c, d = cubic_spline(x3, y3)
-x_interp = np.arange(min(x3), max(x3) + 0.1, 0.1)
-y_interp = evaluate_spline(x3, y3, b, c, d, x_interp)
-tools.plot(x3, y3, x_interp, y_interp, "(Cubic spline) y: [" + '; '.join(str(_) for _ in y3) + "]")
+# plt.plot(x3, y_all, "o")
+# plt.plot(x_interp, y_interp, "red")
+# plt.title("Approximation ({} degree)".format(3))
+# plt.show()
 
 
-# trig. interp.
-y_all = [data[1875 + i][5] for i in range(len(data)) if data[1875 + i][5] != 999.9]
-n = len(y_all)
-x4 = np.linspace(0, n, n)
+# # cubic spline interp.
+# y3 = y
+# x3 = np.linspace(0, 12, 12)
+# b, c, d = cubic_spline(x3, y3)
+# x_interp = np.arange(min(x3), max(x3) + 0.1, 0.1)
+# y_interp = evaluate_spline(x3, y3, b, c, d, x_interp)
+# tools.plot(x3, y3, x_interp, y_interp, "(Cubic spline) y: [" + '; '.join(str(_) for _ in y3) + "]")
 
-x_interp = np.arange(min(x4), max(x4) + 0.1, 0.1)
-coeffs = trig_interp(x4, y_all, np.sin)
-y_interp = [trig_val(coeffs, xi, np.sin) for xi in x_interp]
-tools.plot(x4, y_all, x_interp, y_interp, "(Trig. interp.)")
+
+# # trig. interp.
+# y_all = [data[1875 + i][5] for i in range(len(data)) if data[1875 + i][5] != 999.9]
+# n = len(y_all)
+# x4 = np.linspace(0, n, n)
+
+# x_interp = np.arange(min(x4), max(x4) + 0.1, 0.1)
+# coeffs = trig_interp(x4, y_all, np.sin)
+# y_interp = [trig_val(coeffs, xi, np.sin) for xi in x_interp]
+# tools.plot(x4, y_all, x_interp, y_interp, "(Trig. interp.)")
 
 
 # orth. approx.
@@ -213,7 +213,10 @@ y_all = [data[1875 + i][5] for i in range(len(data)) if data[1875 + i][5] != 999
 n = len(y_all)
 x5 = np.linspace(0, n, n)
 
-coeffs = orth_exp_coeffs(x5, y_all)
-x_interp = np.arange(min(x5), max(x5) + 0.1, 0.1)
+y = [y_all[i] for i in range(0, len(y_all), 20)]
+x6 = [x5[i] for i in range(0, len(x5), 20)]
+
+coeffs = orth_exp_coeffs(x6, y)
+x_interp = np.arange(min(x6), max(x6) + 0.1, 0.1)
 y_interp = orth_exp_approx(x_interp, coeffs)
 tools.plot(x5, y_all, x_interp, y_interp, "(Orth. approx.)")
---
+++ work4/work4.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import spectrogram
+
+
+# DATA 1
+def moving_average(data, window_size):
+    moving_averages = []
+    for i in range(len(data) - window_size + 1):
+        window = data[i:i+window_size]
+        average = np.mean(window)
+        moving_averages.append(average)
+    return np.array(moving_averages)
+
+def DATA1():
+    data = np.fromfile("z1.bin", dtype=np.int16)
+    window_size = 10001
+
+    averages = moving_average(data, window_size)
+    time = np.arange(len(data)) / 1000
+    time_ma = np.arange(len(averages)) / 1000
+
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(time, data)
+    plt.plot(time_ma + window_size / (2 * 1000), averages, 'r-')
+    plt.title('Moving Average')
+    plt.grid(True)
+    plt.show()
+
+
+# DATA 2
+def flat_top_window(n, N):
+    return 1 - 1.93 * np.cos(2 * np.pi * n / N) + 1.29 * np.cos(4 * np.pi * n / N) - 0.388 * np.cos(6 * np.pi * n / N) + 0.032 * np.cos(8 * np.pi * n / N)
+
+def DATA2():
+    data = np.fromfile("z2.bin", dtype=np.double)
+    N = len(data)
+    wind_data = [data[n] * flat_top_window(n, N - 1) for n in range(N)]
+    
+    # spectrum = np.abs(np.fft.fft(wind_data))**2
+    spectrum = np.abs(np.fft.fft(wind_data))
+    f_axe = np.fft.fftfreq(N, 1 / 1000)
+    
+    spectrum, f_axe = np.fft.fftshift((spectrum, f_axe))
+
+    plt.figure(figsize=(10, 6))
+    # plt.semilogy(range(N), spectrum)
+    plt.semilogy(spectrum, f_axe)
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Amplitude")
+    plt.grid(True)
+    plt.show()
+    
+
+# DATA3
+def DATA3():
+    data = np.fromfile("z3.bin", dtype=np.double)
+    N = len(data)
+    
+    # freq, time, spectrogram
+    f, t_spec, Sxx = spectrogram(data, 1000, window=flat_top_window(np.arange(1000), 1000), noverlap=999)
+
+    # Plot the spectrogram
+    plt.figure(figsize=(10, 6))
+    plt.pcolormesh(t_spec, f, 10 * np.log10(Sxx), shading='auto')
+    # plt.colorbar(label='Power Level (dB)')
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [s]')
+    plt.title('Signal Spectrogram with Flat Top Window')
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    # DATA1()
+    # DATA2()
+    DATA3()
